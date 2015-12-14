e1c18731498008e67fa408b8efa1b051d23915b0
ttollers <tom.tollinton@trinitymirror.com>
2015-12-14 19:07:22 +0000

    addede sort and filter

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 
 exports.randomPointInCircle = require('./randomPointInCircle').randomPointInCircle;
 exports.randomPointInCirclesCollection = require('./randomPointInCirclesCollection').randomPointInCirclesCollection;
+exports.sortAndFilter = require('./randomPointInCirclesCollection').sortAndFilter;

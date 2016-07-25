e25cc4cc3653d61bb2a5b1d4a00a0af032e99a09
ttollers <tom.tollinton@trinitymirror.com>
2016-07-25 21:10:47 +0100

    Expose random method

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ const randomPointInCircle = require("./randomPointInCircle");
 
 exports.randomPointInCircle = randomPointInCircle.randomPointInCircle;
 exports.randomPointInSquare = randomPointInCircle.randomWithinSquare;
+exports.randomMinMax = randomPointInCircle.random;
 exports.randomPointInCirclesCollection = require('./randomPointInCirclesCollection').randomPointInCirclesCollection;
 exports.sortAndFilter = require('./randomPointInCirclesCollection').sortAndFilter;
